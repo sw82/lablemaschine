b911f29a09e6f2897a8b0268d7f3bd0f29b6eeaa
sw82 <swinkler@informatik.hu-berlin.de>
2015-07-08 23:03:11 +0200

    more wise words

--- start.py
+++ start.py
@@ -12,17 +12,30 @@ taglines = [
     'Gib diesen Zettel weiter an eine Person, der du deine Wohnung untervermieten würdest, so lange du im Urlaub bist.',
 	'Gib diesen Zettel weiter an eine Person, der du 1000 Euro leihen würdest.',
 	'Gib diesen Zettel weiter an jemanden, den du überhaupt nicht einschätzen kannst.',
-	'Setze dich neben die Person, die dir hier im  Raum am ähnlichsten ist, oder in ihre Nähe, und gebe ihr dann diesen Zettel.',
+	'Setze dich neben die Person, die dir hier im am ähnlichsten ist, oder in ihre Nähe, und gebe ihr dann diesen Zettel.',
 	'Gib diesen Zettel weiter an die Person, die am glücklichsten aussieht.',
 	'Gib diesen Zettel der Person, der du am ehesten zutraust ein politisches Amt übernehmen zu können.',
 	'Gib diesen Zettel weiter an die Person, von der du denkst, dass sie am meisten Freunde hat.',
 	'Gib diesen Zettel jemandem, den du für verdächtig hälst.',
 	'Gib diesen Zettel einer Person, die du nicht kennst, aber gerne kennenlernen möchtest.',
 	'Gehe in die Nähe einer Person die dir sehr sympathisch ist und gebe diesen Zettel dann weiter an eine andere Person in eurer Nähe.',
-	'Wenn Sie mehr als 50 Euro Bargeld dabei haben, geben Sie jemandem einen Drink aus',
-	'Wenn Sie v. a. aus beruflichen Gründen hier sind, finden sie jemanden der Sie herumführt',
+	'Wenn Du mehr als 50 Euro Bargeld dabei hast, gib jemandem einen Drink aus',
+	'Wenn Du v. a. aus beruflichen Gründen hier sind, frage Dich warum Du hier bist',
 	'Willst Du heute noch jemanden kennen lernen dann halte Dich an der Bar auf!'
     ]
+
+wisewords = [
+    'Das Gras wächst nicht schneller, wenn man daran zieht!',
+    'Die Menschen stolpern nicht über Berge, sondern über Maulwurfshügel. Begradige einen!',
+    'Stille Wasser sind auch nass. Hol Dir eins an der Bar',
+    'Heute schon gespendet?'
+]
+
+advertising =[
+    'Paczka 2015',
+    'brainslug.me',
+    'Feldversuch 2015'
+]
 
 lengthoftaglines = len(taglines)
 running = True
