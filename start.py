2bbd4fbb8f7c759b3f49387e1d478f8883eb61ce
sw82 <swinkler@informatik.hu-berlin.de>
2015-07-17 11:09:31 +0200

    generic usb import

--- start.py
+++ start.py
@@ -5,7 +5,7 @@
 # Copyright 2015 Sebastian Winkler
 # http://sbstnwnklr.com
 
-import os, sys, time, datetime, subprocess, usb.core, usb.util, Button
+import os, sys, time, datetime, subprocess, usb, Button #usb.core, usb.util
 from PIL import Image, ImageFont, ImageDraw
 
 # TODO
