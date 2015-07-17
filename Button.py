8ff1d627d59dd635cedff4c3db13e6817da1dfe0
sw82 <swinkler@informatik.hu-berlin.de>
2015-07-17 11:28:15 +0200

    comment changed

--- Button.py
+++ Button.py
@@ -13,6 +13,7 @@ import usb.core
 class Button:
   def __init__(self):
     # Device is: Bus 001 Device 008: ID 0d50:0141 Cleware GmbH
+
     self.dev = usb.core.find(idVendor=0x0d50, idProduct=0x0141)
     if not self.dev:
       raise ValueError("Button not found")
@@ -23,9 +24,8 @@ class Button:
       pass # already unregistered
 
   def read(self):
-    """ Read the USB port.
-    Return 1 if pressed and released, 0 otherwise.
-    """
+    # Read the USB port. Return 1 if pressed and released, 0 otherwise.
+
     #Magic numbers are from http://search.cpan.org/~bkendi/Device-USB-PanicButton-0.04/lib/Device/USB/PanicButton.pm
     return self.dev.ctrl_transfer(bmRequestType=0xA1, bRequest=1, wValue=0x300, data_or_wLength=8, timeout=500)[0]
 
