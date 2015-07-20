ec23b6ad43790349ae37a6b171585a64afb4228c
sw82 <swinkler@informatik.hu-berlin.de>
2015-07-20 22:55:27 +0200

    more config output

--- ButtonTest.py
+++ ButtonTest.py
@@ -12,13 +12,17 @@ if dev is None:
 
 for cfg in dev:
     sys.stdout.write(str(cfg.bConfigurationValue) + '\n')
-    for intf in cfg:
-        sys.stdout.write('\t' + \
-                         str(intf.bInterfaceNumber) + \
-                         ',' + \
-                         str(intf.bAlternateSetting) + \
-                         '\n')
-        for ep in intf:
-            sys.stdout.write('\t\t' + \
-                             str(ep.bEndpointAddress) + \
-                             '\n')
+    sys.stdout.write(str(cfg.bLength) + '\n')
+    sys.stdout.write(str(cfg.bNumConfigurations) + '\n')
+    sys.stdout.write(str(cfg.bDeviceClass) + '\n')
+
+    # for intf in cfg:
+    #     sys.stdout.write('\t' + \
+    #                      str(intf.bInterfaceNumber) + \
+    #                      ',' + \
+    #                      str(intf.bAlternateSetting) + \
+    #                      '\n')
+    #     for ep in intf:
+    #         sys.stdout.write('\t\t' + \
+    #                          str(ep.bEndpointAddress) + \
+    #                          '\n')
