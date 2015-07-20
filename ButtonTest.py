8a38c0d505fc79bf4cd6be8d6926ae9ae6ec5c2c
sw82 <swinkler@informatik.hu-berlin.de>
2015-07-20 22:43:23 +0200

    new button test file added (playground)

---
+++ ButtonTest.py
@@ -0,0 +1,10 @@
+# USB  Button test file
+# Copyright 2015 Sebastian Winkler
+# http://sbstnwnklr.com
+#
+
+import usb.core
+
+dev = usb.core.find(idVendor=0x0d50, idProduct=0x0141)
+if dev is None:
+    raise ValueError('Our device is not connected')
