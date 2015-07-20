f3662115b8fbcbf2fb0e7043f657a9aa05d08d0c
sw82 <swinkler@informatik.hu-berlin.de>
2015-07-20 22:49:23 +0200

    added even more output

--- ButtonTest.py
+++ ButtonTest.py
@@ -12,3 +12,13 @@ if dev is None:
 
 for cfg in dev:
     sys.stdout.write(str(cfg.bConfigurationValue) + '\n')
+    for intf in cfg:
+        sys.stdout.write('\t' + \
+                         str(intf.bInterfaceNumber) + \
+                         ',' + \
+                         str(intf.bAlternateSetting) + \
+                         '\n')
+        for ep in intf:
+            sys.stdout.write('\t\t' + \
+                             str(ep.bEndpointAddress) + \
+                             '\n')
